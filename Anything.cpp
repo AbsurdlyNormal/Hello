f725093d075c24bd25f92785a71139fb7d28c273
Ishit-1290 <64848823+Ishit-1290@users.noreply.github.com>
2021-06-29 15:13:49 +0530

    Beans and Pizza added

--- Anything.cpp
+++ Anything.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 int main(){
   std::cout << "HELLO WORLD\n";
-  std::cout << "CHicken Fry";
+  std::cout << "CHicken Fry\n";
+  std::cout << "BEANS\n";
+  std::cout << "Pizza\n\n";
   std::cout << "Thank you";
 }
